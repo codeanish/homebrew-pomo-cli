484b62567a66c68fe67649181b8e7e6456b51d8b
codeanish <anish@codeanish.com>
2023-09-27 07:15:46 -0700

    Brew formula update for pomo version v0.1.5

--- Formula/pomo-cli.rb
+++ Formula/pomo-cli.rb
@@ -5,21 +5,21 @@
 class PomoCli < Formula
   desc "Pomodoro Timer CLI tool"
   homepage "https://github.com/codeanish/homebrew-pomo-cli"
-  version "0.1.4"
+  version "0.1.5"
   depends_on :macos
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/codeanish/pomo-cli/releases/download/v0.1.4/pomo_Darwin_arm64.tar.gz"
-      sha256 "05d14387896c59d42c35534231f39bf00a71dc5ff5abe8e7faca41c166d9e040"
+    if Hardware::CPU.intel?
+      url "https://github.com/codeanish/pomo-cli/releases/download/v0.1.5/pomo_Darwin_x86_64.tar.gz"
+      sha256 "2be0b30b92a7adb9e4d27c452aed671ffa043d2ee04861e6dd4a17cfb709a940"
 
       def install
         bin.install "pomo"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/codeanish/pomo-cli/releases/download/v0.1.4/pomo_Darwin_x86_64.tar.gz"
-      sha256 "6c6f597b6e5a4d1486a0e013030abedd50aa06659c9e30e4f2736dc54758784c"
+    if Hardware::CPU.arm?
+      url "https://github.com/codeanish/pomo-cli/releases/download/v0.1.5/pomo_Darwin_arm64.tar.gz"
+      sha256 "c73f837733a3556d377cbbea5c2dd6fc0a93a152e1afea52cbb04a2947a54a96"
 
       def install
         bin.install "pomo"
